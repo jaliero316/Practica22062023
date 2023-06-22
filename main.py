dd8576a0bffc93033c68baa0d4ca9b239644ef4a
javier.hermosa <javier.hermosa@espoir.org.ec>
2023-06-22 12:09:30 -0500

    .env

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from Telegram_bot import TelegramBot
-from mo
+from mongodb import MongoDB
+chrome_driver = webdriver.Chrome()
+
+bot = TelegramBot()
+db = MongoDB()
+
+chrome_driver.get("https://www.wikipedia.org/")
+print(chrome_driver.title)
--- mongodb.py
+++ mongodb.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class MongoDB:
     def __int__(self):
         mongo_user = os.getenv("MONGO_USER")
@@ -13,4 +14,17 @@ class MongoDB:
         self.collection = os.getenv("COLLECTION_NAME")
         mongo_hostname = os.getenv("MONGO_HOSTNAME")
 
-        uri = f"mongodb+srv://{mongo_user}:{mongo_pass}@{mongo_hostname}/?retryWhites=true&w=majority"
+        uri = f"mongodb+srv://{mongo_user}:{mongo_pass}@{mongo_hostname}/?retryWrites=true&w=majority"
+        # Crea un nuevo cliente y conecta hacia el servidor#
+        self.client = MongoClient(uri)
+        # Envia un ping de confirmacion de que se ha logrado la conexion #
+        try:
+            self.client.admin.command('ping')
+            print("Pinged your deployement. You succesfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
+
+
+def insert_wikipedia_text(self, title: str, text: str):
+    eig_db = self.client.get_database("eig")
+    col = eig_db.get_co
